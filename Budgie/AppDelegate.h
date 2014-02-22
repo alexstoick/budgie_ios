98e79ccf596b43a69ace6b69cc43c7b1c4083a1d
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2014-02-22 16:01:56 +0000

    Initial Commit

---
+++ Budgie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Budgie
+//
+//  Created by Stoica Alexandru on 2/22/14.
+//  Copyright (c) 2014 Stoica Alexandru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
